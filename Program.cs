24c712118a16cdf589081e07929a43792b4136bf
Anil <anil>
2024-07-10 18:24:37 +0530

    add references

--- Program.cs
+++ Program.cs
@@ -5,41 +5,14 @@ using BotRepository;
 using ForwardTraderBot.Service;
 using Services.Interfaces.V2;
 using Services.V2;
+using InfrastructureUtil;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddHttpClient<ISolanaQuickNodeMainetApiService, SolanaQuickNodeMainetApiService>("SolanaMainNet", httpClient =>
-{
-    httpClient.BaseAddress = new Uri(builder.Configuration.GetValue<string>("AppSettings:SolanaRPCEndPoint"));
-}).AddPolicyHandler(httpRequestMessage => BuilderUtil.GetTimeOutPolicy())
-.AddPolicyHandler(BuilderUtil.GetRetryPolicy());
-
-builder.Services.AddHttpClient<IJupiterService, JupiterService>("JupiterHttpClient", httpClient =>
-{
-    httpClient.BaseAddress = new Uri(builder.Configuration.GetValue<string>("AppSettings:JupiterEndPoint"));
-}).AddPolicyHandler(httpRequestMessage => BuilderUtil.GetTimeOutPolicy())
-.AddPolicyHandler(BuilderUtil.GetRetryPolicy());
-
-builder.Services.AddHttpClient<IJupiterService, JupiterService>("TransactionClient", httpClient =>
-{
-    httpClient.BaseAddress = new Uri(builder.Configuration.GetValue<string>("AppSettings:TransactionEndPoint"));
-}).AddPolicyHandler(httpRequestMessage => BuilderUtil.GetTimeOutPolicy())
-.AddPolicyHandler(BuilderUtil.GetRetryPolicy());
-
-
-builder.Services.AddControllers();
-
-
+builder.ConfigureBuilder();
+builder.Services.ConfigureServices(builder);
 builder.Services.AddHostedService<ReverseTraderService>();
-builder.Services.AddTransient<ICoinCheckServiceV2, CoinCheckServiceV2>();
-builder.Services.AddTransient<ICoinRepo, CoinRepo>();
-builder.Services.AddTransient<IBotInstanceRepo, BotInstanceRepo>();
-builder.Services.AddTransient<IBirdEyeService, BirdEyeService>();
-builder.Services.AddTransient<IRPCService, RPCService>();
-builder.Services.AddTransient<ITransactionRepo, TransactionRepo>();
-builder.Services.AddTransient<IJupiterService, JupiterService>();
-builder.Services.AddTransient<IBlockChainApiService, BlockChainApiService>();
-builder.Services.AddTransient<ITransactionService, TransactionService>();
+
 
 var app = builder.Build();
 
--- Service/ReverseTraderService.cs
+++ Service/ReverseTraderService.cs
@@ -1,8 +1,12 @@
 ﻿
+using BlockChainServices.Interfaces;
 using BotModels;
 using BotModels.Repository;
+using BotModels.Response.HascorpVault;
 using BotServices;
+using CoinSwapper;
 using Services.Interfaces.V2;
+using Utility;
 
 namespace ForwardTraderBot.Service
 {
@@ -12,24 +16,59 @@ namespace ForwardTraderBot.Service
         private Timer? _timer = null;
         private readonly AppSettings _appSettings;
         private readonly ICoinCheckService _coinService;
+        private readonly IAuthService _authService;
+        private readonly IBlockChainApiService _blockChainApiService;
+        private readonly IConfiguration _configuration;
 
         private readonly ICoinCheckServiceV2 _coinServiceV2;
-        public ReverseTraderService(ILogger<ReverseTraderService> logger, IConfiguration configuration, ICoinCheckServiceV2 coinCheckServiceV2)
+        public ReverseTraderService(ILogger<ReverseTraderService> logger, IConfiguration configuration, ICoinCheckServiceV2 coinCheckServiceV2,
+           IAuthService authService, IBlockChainApiService blockChainApiService)
         {
             _logger = logger;
             _appSettings = configuration.GetSection("AppSettings").Get<AppSettings>();
             _coinServiceV2 = coinCheckServiceV2;
+            _authService = authService;
+            _blockChainApiService = blockChainApiService;
+            _configuration = configuration;
 
         }
 
-        public Task StartAsync(CancellationToken cancellationToken)
+        public async Task StartAsync(CancellationToken cancellationToken)
         {
-            _logger.LogInformation("ReverseTraderService Service running.");
+            List<SecretDto> vaultKeys = await _authService.GetSecrets(_appSettings.vaultProjectKey, _appSettings.identifier, true);
+
+            foreach (var vaultKey in vaultKeys)
+            {
+                if (vaultKey.key.Contains("wallet_key"))
+                {
+                    try
+                    {
+                        var account = WalletExtensions.Account(vaultKey.value);
+                        if (account != null && !string.IsNullOrEmpty(account.PublicKey))
+                        {
+                            vaultKey.publicKey = account.PublicKey;
+                        }
+                        else
+                        {
+                            //incase failed to convert
+                            vaultKey.publicKey = await _blockChainApiService.GetPublicKey(vaultKey.value);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        LoggerUtil.LogError(_logger, "Error handling key conversion", exception: ex);
+                    }
+                }
+
+            }
+            CacheUtil.vaultSecrets = vaultKeys;
+            _configuration["ConnectionStrings:CryptoDbConnection"] = CacheUtil.vaultSecrets.Where(x => x.key == "DbConnection").Select(x => x.value).FirstOrDefault();
+            LoggerUtil.LogInformation(_logger, "ReverseTraderService Service running.");
 
             _timer = new Timer(DoWork, null, TimeSpan.Zero,
                 TimeSpan.FromSeconds(_appSettings.botinterval));
 
-            return Task.CompletedTask;
+            //return Task.CompletedTask;
         }
 
 
@@ -55,7 +94,7 @@ namespace ForwardTraderBot.Service
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            _logger.LogInformation("ReverseTraderService is stopping.");
+            LoggerUtil.LogInformation(_logger, "ReverseTraderService is stopping.");
 
             _timer?.Change(Timeout.Infinite, 0);
 
